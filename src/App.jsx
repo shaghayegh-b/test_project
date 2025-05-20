9f20ec21af97cd0aaab611efbe19969094043a5a
shaghayegh <bazrafkan.sh@gmail.com>
2025-05-20 22:40:03 +0330

    commit?

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-
-
 function App() {
   return (
     <div>
-            <h1 className="p-9 bg-red-400">hiii</h1>
-    <h3>fff</h3>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, pariatur dicta tempora repellendus beatae eveniet consequuntur alias voluptatem, dolore voluptatum molestias cum facere vel ratione numquam culpa excepturi doloribus delectus.</p>
-    <h2>ddd</h2>
+      <h1 className="p-9 bg-red-400">hiii</h1>
+      <h3>fff</h3>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
