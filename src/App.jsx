caee9329e2090c186f3805f0b3a60680e4f84832
shaghayegh <bazrafkan.sh@gmail.com>
2025-05-20 22:38:11 +0330

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
     <div>
             <h1 className="p-9 bg-red-400">hiii</h1>
     <h3>fff</h3>
-
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, pariatur dicta tempora repellendus beatae eveniet consequuntur alias voluptatem, dolore voluptatum molestias cum facere vel ratione numquam culpa excepturi doloribus delectus.</p>
+    <h2>ddd</h2>
     </div>
   )
 }
